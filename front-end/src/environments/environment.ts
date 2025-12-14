80ceb646244d63972c76c8922c6ecbb4017f8ff0
graven <shinominecro@gmail.com>
2025-12-15 02:56:20 +0800

    editing

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api',
+    apiUrl: 'http://localhost:8000/api',  // Laravel backend running in Docker
+    appName: 'RollMate',
+    version: '1.0.0'
   };
